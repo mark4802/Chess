2432031c221cebc37fcabe4de4ef2d777c2c29cf
Mark <markjessing01@gmail.com>
2025-12-04 19:29:25 +0100

    completed move logic (prett much) still can make illegal moves etc. but basic move logic and move log implemented

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -32,11 +32,31 @@ def main():
     gs = ChessEngine.GameState()
     loadImages()#only once before loop
     running = True
+    sqSelected = ()# No square is selected as default
+    playerClicks = [] # Keep track of player clicks (eg. [(6, 4), (4, 4)])
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
 
+                if sqSelected == (row, col): # You clicked the same square twice
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2: # This is the second click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = () # Reset user clicks
+                    playerClicks = []
+
+                    
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -53,17 +73,17 @@ def drawGameState(screen, gs):
 def drawBoard(screen):
     colors = [p.Color("white"), p.Color("gray")]
     for row in range(DIMENSION):
-        for column in range(DIMENSION):
-            color = colors[((row+column) % 2)]
-            p.draw.rect(screen, color, p.Rect(column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+        for col in range(DIMENSION):
+            color = colors[((row+col) % 2)]
+            p.draw.rect(screen, color, p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 def drawPieces(screen, board):
     for row in range(DIMENSION):
-        for column in range(DIMENSION):
-            piece = board[row][column]
+        for col in range(DIMENSION):
+            piece = board[row][col]
             if piece != "--": # not empty square
-                screen.blit(IMAGES[piece], p.Rect(column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 if __name__ == "__main__":
     main()
